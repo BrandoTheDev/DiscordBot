e0190dd4715fe9ef3cabb6189da799e86a37ef03
BrandoTheDev <brandothedev@gmail.com>
2022-04-06 13:37:20 -0500

    Successful connection to discord

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,2 +1,14 @@
+import Utils.KeyHandler;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+import javax.security.auth.login.LoginException;
+
 public class Launcher {
+
+    public static void main(String[] args) throws LoginException {
+
+        JDA jda = JDABuilder.createDefault(KeyHandler.discordKey)
+                .build();
+    }
 }
